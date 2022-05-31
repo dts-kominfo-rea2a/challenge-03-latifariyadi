547ac99b794c33bba216e46c08e67c3e9ff234f6
Latif Ariyadi <latifariyadi86@gmail.com>
2022-06-01 02:29:40 +0700

    first commit

--- index.js
+++ index.js
@@ -1,46 +1,59 @@
 // ! JANGAN DIMOFIDIKASI
-const dataBelanjaan = [
-  {
-    id: "D-10001",
-    nama: "Minyak Goreng Delima",
-    harga: 25000,
-    kuantitas: 2,
-  },
-  {
-    id: "D-10020",
-    nama: "Beras Mamos",
-    harga: 48000,
-    kuantitas: 1,
-  },
-  {
-    id: "F-00010",
-    nama: "Larutan Cap Kaki Empat",
-    harga: 7500,
-    kuantitas: 8,
-  },
+const dataBelanjaan = [{
+        id: "D-10001",
+        nama: "Minyak Goreng Delima",
+        harga: 25000,
+        kuantitas: 2,
+    },
+    {
+        id: "D-10020",
+        nama: "Beras Mamos",
+        harga: 48000,
+        kuantitas: 1,
+    },
+    {
+        id: "F-00010",
+        nama: "Larutan Cap Kaki Empat",
+        harga: 7500,
+        kuantitas: 8,
+    },
 ];
 
 // boleh dimodifikasi bila ingin menggunakan deklarasi fungsi yang normal
-const listBelanjaan = null;
+// const listBelanjaan = null;
+const listBelanjaan = (data) => {
+    const hasil = [];
+
+    for (let index = 0; index < data.length; index++) {
+        hasil.push("- "+data[index].nama+" x "+data[index].kuantitas);
+    }
+    return hasil;
+};
 
 // boleh dimodifikasi bila ingin menggunakan deklarasi fungsi yang normal
-const totalBelanjaan = null;
+const totalBelanjaan = (data) => {
+    let hasil = null;
+    for (let index = 0; index < data.length; index++) {
+        hasil = data[index].harga * data[index].kuantitas + hasil;
+    }
+    return hasil;
+}
 
 // ! JANGAN DIMODIFIKASI
 const main = () => {
-  console.log("Selamat datang customer");
+    console.log("Selamat datang customer");
 
-  console.log("Belanjaan Anda adalah:");
-  console.log(listBelanjaan?.(dataBelanjaan)?.join("\n"));
-  console.log(
-    "\nTotal Belanjaan Anda adalah Rp. " + totalBelanjaan?.(dataBelanjaan)
-  );
+    console.log("Belanjaan Anda adalah:");
+    console.log(listBelanjaan ?.(dataBelanjaan) ?.join("\n"));
+    console.log(
+        "\nTotal Belanjaan Anda adalah Rp. " + totalBelanjaan ?.(dataBelanjaan)
+    );
 };
 
 main();
 
 module.exports = {
-  dataBelanjaan,
-  listBelanjaan,
-  totalBelanjaan,
+    dataBelanjaan,
+    listBelanjaan,
+    totalBelanjaan,
 };
